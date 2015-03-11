a28917d06b64b4f0dd2d6c6573b2691266298ea4
Dima-Git <urmemoris@gmail.com>
2015-03-11 15:51:01 +0300

    Added forks handling.

--- threads.c
+++ threads.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
-	#ifdef THREADS
-	printf("Threads.\n");
-	#else
-	printf("Processes.\n");
-	#endif
+	for (;;) {
+		int x = 0;
+		scanf("%d", &x);
+		#ifdef THREADS
+		printf("Threads.\n");
+		#else
+		if (x == 0) {
+			break;
+		} else {
+			switch (fork()) {
+			case -1 :
+				printf("Fork error!\n");
+				break;
+			case  0 :
+				sleep(3);
+				printf("Child process.\n");
+				return 0;
+			default :
+				printf("Main process.\n");
+			}
+		}
+		#endif
+	}
 	return 0;
 }
