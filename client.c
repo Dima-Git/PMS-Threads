3a9bcfd8a1f88b4c2cb1b1828fa8e1bca6802ff9
Dima-Git <urmemoris@gmail.com>
2015-03-19 15:02:57 +0300

    Files sending now by parts, and saving

--- client.c
+++ client.c
@@ -6,53 +6,63 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 
+#define BUFFER_SIZE 256
+
 int main(int argc, char *argv[])
 {
-	int sock;
-	char s[70], * sfile;
-	int sz, szfile;
+	FILE * file;
+	int sock, sz, szpart, exist;
+	char s[170], buffer[BUFFER_SIZE];
 	struct sockaddr_in serv_addr;
 	
-	/* socket */
+	/* args check */
+	if (argc != 3) {
+		printf("Args: host port.\n");
+		return 0;
+	}
+
+	/* * socket * */
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1) {
 		printf("Socket creation error.\n");
 		return 0;
 	}
-	
+
 	/* serv_addr */
 	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr(argv[1]);
         serv_addr.sin_port = htons(atoi(argv[2]));
-	
+
 	/* bind */
 	if (connect(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
 		printf("Socket connection error.\n");
 		close(sock);
 		return 0;
 	}
-	
-	/* interaction */
+
+	/* * interaction * */
 	/* send path */
+	printf("Input file name : ");
 	scanf("%s", s);
 	sz = strlen(s) + 1;
 	write(sock, &sz, sizeof(int));
 	write(sock, s, sz);
-	
+
 	/* recieve file */
-	read(sock, &szfile, sizeof(int));
-	if (szfile != -1) {
-		sfile = (char *)malloc(szfile);
-		read(sock, sfile, szfile);
-		printf("File :\n%s", sfile);
-		free(sfile);
+	read(sock, &exist, sizeof(int));
+	if (exist) {
+		file = fopen(s, "wb");
+		do {
+			szpart = read(sock, buffer, BUFFER_SIZE);
+			fwrite(buffer, sizeof(char), szpart, file);
+		} while (szpart > 0);
+		/* close */
+		close(file);
 	} else {
-		printf("No file.\n");
+		printf("Requested file not found on server.\n");
 	}
-	
-	/* close */
 	close(sock);
-	
+
 	return 0;
 }
--- threads.c
+++ threads.c
@@ -8,48 +8,41 @@
 #include <netinet/in.h>
 #include <netinet/ip.h>
 
+#define BUFFER_SIZE 256
+
 static void * routine(void * args)
 {
+	char buffer[BUFFER_SIZE];
 	int sock = *(int *)args;
-	int szpath, szfile = -1, i = 0;
-	char * spath, * sfile, c;
+	int szpath, szpart = 0, exist;
+	char * spath;
 	FILE * file;
-	
-	/*sleep(3); */
-	printf("Child routine. Args : %d\n", sock);
-	
+
+	printf("Client conected. sockfd : %d\n", sock);
+
 	/* recieve path */
 	read(sock, &szpath, sizeof(int));
-	printf("Given size : %d\n", szpath);
 	spath = calloc(szpath, sizeof(char));
 	read(sock, spath, szpath);
-	printf("Given string : %s\n", spath);
-	
-	/* read file */
-	file = fopen(spath, "r");
+	printf("Requested file : %s\n", spath);
+
+	/* read and send file */
+	file = fopen(spath, "rb");
 	if (file) {
-		fseek(file, 0, SEEK_END);
-		szfile = ftell(file);
-		rewind(file);
-		sfile = (char *)calloc(szfile + 1, sizeof(char));
-		printf("Size : %d\n", szfile);
-		while (fscanf(file, "%c", &c) == 1) {
-			sfile[i] = c;
-			++ i;
+		exist = 1;
+		write(sock, &exist, sizeof(int));
+		while ((szpart = fread(buffer, sizeof(char), BUFFER_SIZE, file)) > 0) {
+			write(sock, buffer, szpart);
 		}
-	} else {
-	}
-	/* send file */
-	write(sock, &szfile, sizeof(int));
-	if (szfile != -1) {
-		write(sock, sfile, szfile);
-	}
-	
-	/* clean */
-	if (file) {
-		free(sfile);
 		fclose(file);
+	} else {
+	/* file not found */
+		exist = 0;
+		write(sock, &exist, sizeof(int));
+		printf("Requested file not found.\n");
 	}
+
+	/* clean */
 	free(spath);
 	free(args);
 	close(sock);
@@ -58,22 +51,26 @@ static void * routine(void * args)
 
 static int create_tcp_socket(char * port)
 {
-	int sock;
+	int sock, optval = 1;
 	struct sockaddr_in serv_addr;
-	
+
 	/* socket */
 	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1) {
 		printf("Socket creation error.\n");
 		return -1;
 	}
-	
+
 	/* serv_addr */
 	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(atoi(port));
 	serv_addr.sin_addr.s_addr= htonl(INADDR_ANY);
-	
+
+	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof optval) == -1) {
+		printf("setsockopt error.\n");
+		return -1;
+	}
 	/* bind */
 	if (bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
 		printf("Socket bindind error.\n");
@@ -88,29 +85,33 @@ static int create_tcp_socket(char * port)
 
 int main(int argc, char *argv[])
 {
-	int sock, i;
-	
+	int sock;
+
+	if (argc != 2) {
+		printf("Args: port.\n");
+		return 0;
+	}
+
 	/* socket bind listen */
 	sock = create_tcp_socket(argv[1]);
 	if (sock == -1)
 		return 1;
 	
-	for (i = 0; i < 3; ++ i) {
+	for (;;) {
 		/* accept */
 		int * pclient_sock = (int *)malloc(sizeof(int));
 		struct sockaddr_in client_addr;
 		pthread_t pt;
-		int p;
-		
 		socklen_t client_len = sizeof(client_addr);
+		
 		*pclient_sock = accept(sock, (struct sockaddr *)&client_addr, &client_len);
 		if (*pclient_sock == -1) {
 			printf("Socket accept error.\n");
 		}
-		
+
 		/* interaction */
 #ifdef THREADS
-		p = pthread_create(&pt, 0, routine, pclient_sock);
+		pthread_create(&pt, 0, routine, pclient_sock);
 #else
 		switch (fork()) {
 		case -1 :
@@ -118,15 +119,17 @@ int main(int argc, char *argv[])
 			break;
 		case  0 :
 			routine(pclient_sock);
-			return 0;
+			break;
 		default :
+			close(*pclient_sock);
+			free(pclient_sock);
 			break;
 		}
 #endif
 	}
-	
-	/* close & unlink */
+
+	/* close */
 	close(sock);
-	
+
 	return 0;
 }
